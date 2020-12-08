2d5cf79e57948f70c3802962458d720d4e26fc27
NDH <nathanhill123@gmail.com>
2020-12-08 15:30:43 -0500

    latest version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
-let input;
+
 
 // Write your code below:
 
@@ -11,12 +11,14 @@ const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 };
 
+let targetNumber = generateTarget();
+
 // compares the different inputs and sees which one is closer to the target
+/*
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
 
-const compareGuesses = (humanGuess, computerGuess, generateTarget) => {
-
-    let userDiff = generateTarget - humanGuess;
-    let botDiff = generateTarget - computerGuess;
+    let userDiff = targetNumber - humanGuess;
+    let botDiff = targetNumber - computerGuess;
     let input;
     
     if (userDiff > botDiff) {
@@ -60,48 +62,35 @@ const compareGuesses = (humanGuess, computerGuess, generateTarget) => {
     };
 
 };
+*/
 
-// allows us to change the input from true & false to "human" & "computer"
+let compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+    const userG = Math.abs(targetNumber - humanGuess);
+    const computerG = Math.abs(targetNumber - computerGuess);
+    return userG <= computerG;
 
 /*
-function changeInput() {
-    if (compareGuesses() === true) {
-        input = "human";
-    } else {
-        input = "computer";
-    };
-};
+
+// changes value of input to either "human" or "computer"
+let input =  compareGuesses(humanGuess, computerGuess, targetNumber) ? 'human' : 'computer';
 */
 
 // updates scoreboard by increasing the score depending on who wins (++)
 
-const updateScore = () => {
+const updateScore = (input) => {
 
-    if (compareGuesses()) {
-        return humanScore += 1;
+    if (input === "human") {
+        return ++humanScore;
         
-    } else if (!compareGuesses()) {
-        return computerScore += 1;
-
-    } else {
-        console.log("There is no winner.");
+    } else if (input === "computer") {
+        return ++computerScore;
     };
-}
+};
 
 // updates the rounds by increasing the currentRound after each run through (++)
 
 function advanceRoundNumber() {
-    if ((input === true) || (input === false)) {
-        return currentRoundNumber ++;
-    };
-}
+    currentRoundNumber ++;
+};
 
 
-
-// calling the functions to test how they are working so far
-
-// console.log(input);                                                      // var never changes - it should shift between "computer" & "human"
-// console.log(compareGuesses(3, 3, generateTarget()));                     // seems to work
-// console.log(updateScore());                                              // returns There is no winner
- console.log(advanceRoundNumber());                                       // undefined
-// console.log(humanScore, computerScore, currentRoundNumber);              // score stays the same
